9a18cafd68b2e8314e37bcdc0cbf676e24e69eda
dlogan <dlogan@169.254.80.80>
2015-06-29 21:10:24 -0400

    Merge current changes from VA camp.

--- CTFd/config.py
+++ CTFd/config.py
@@ -6,18 +6,18 @@ SESSION_TYPE = "filesystem"
 SESSION_FILE_DIR = "/tmp/flask_session"
 SESSION_COOKIE_HTTPONLY = True
 PERMANENT_SESSION_LIFETIME = 604800 # 7 days in seconds
-HOST = ".ctfd.io"
+HOST = ".uscyberchallenge.org"
 UPLOAD_FOLDER = os.path.normpath('static/uploads')
 
 ##### EMAIL (Mailgun and non-Mailgun) #####
 
 # The first address will be used as the from address of messages sent from CTFd
-ADMINS = []
+ADMINS = ["dlogan@cyberninjas.com"]
 
 ##### EMAIL (if not using Mailgun) #####
-CTF_NAME = ''
-MAIL_SERVER = ''
-MAIL_PORT = 0
+CTF_NAME = 'USCC 2015 - CTF'
+MAIL_SERVER = '127.0.0.1'
+MAIL_PORT = 25
 MAIL_USE_TLS = False
 MAIL_USE_SSL = False
 MAIL_USERNAME = ''
